d9f549f0b1b303315479b5077430092591c8d9e1
hoangthien1101 <hoangthien110104@gmail.com>
2024-08-22 12:47:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // document.getElementById('introduction').textContent = "Hello! I am John Doe, a passionate Web Developer with experience in creating dynamic and responsive websites.";
+    document.getElementById('hobby1').textContent = "Photography";
+    document.getElementById('hobby2').textContent = "Coding";
+    document.getElementById('hobby3').textContent = "Traveling";
+    document.getElementById('email').textContent = "hoangthien110104@gmail.com";
+});
